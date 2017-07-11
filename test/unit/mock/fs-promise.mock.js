b786d27ff1ddfa0f3bf969a6de20fe571e71f92a
Rowan Manning <accounts@rowanmanning.co.uk>
2017-07-11 09:38:08 +0100

    Add in image verification (#9)
    This mostly gives us a way to verify SVGs. Currently it asserts that
    there are no width and height attributes on the root node.

--- test/unit/mock/fs-promise.mock.js
+++ test/unit/mock/fs-promise.mock.js
@@ -6,5 +6,6 @@ require('sinon-as-promised');
 module.exports = {
 	createReadStream: sinon.stub(),
 	readdir: sinon.stub().resolves(),
+	readFile: sinon.stub().resolves(),
 	writeFile: sinon.stub().resolves()
 };
