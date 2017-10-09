74ad8eed64414cb61a0b8786b485fc4fbeb2d455
Jake Champion <me@jakechampion.name>
2017-10-09 16:05:24 +0100

    Update origami-image-set-tools.js

--- test/unit/lib/origami-image-set-tools.js
+++ test/unit/lib/origami-image-set-tools.js
@@ -78,7 +78,7 @@ describe('lib/origami-image-set-tools', () => {
 		});
 
 		it('has a `version` property', () => {
-			assert.strictEqual(OrigamiImageSetTools.defaults.version, '0');
+			assert.strictEqual(OrigamiImageSetTools.defaults.version, 'v0.0.0');
 		});
 
 		it('has a `imageServiceApiKey` property', () => {
@@ -948,8 +948,8 @@ describe('lib/origami-image-set-tools', () => {
 
 			describe('instance', () => {
 
-				it('has a `version` property set to "0"', () => {
-					assert.strictEqual(instance.version, '0');
+				it('has a `version` property set to "v0.0.0"', () => {
+					assert.strictEqual(instance.version, 'v0.0.0');
 				});
 
 			});
