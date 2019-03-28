b756cbb3527c580e4d98b04af4613905f5778040
Jake Champion <jakechampion.jake2@googlemail.com>
2019-03-28 14:48:10 +0000

    only turn on the skip/pending/only rules when running in CI

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -6,14 +6,14 @@ module.exports = {
         "mocha",
     ],
     "rules": {
-        "mocha/no-exclusive-tests": "error",
+        "mocha/no-exclusive-tests": process.env.CI ? "error" : "warn",
         "mocha/no-global-tests": "error",
         "mocha/no-mocha-arrows": "error",
-        "mocha/no-pending-tests": "error",
+        "mocha/no-pending-tests": process.env.CI ? "error" : "warn",
         "mocha/no-return-and-callback": "error",
         "mocha/no-setup-in-describe": "warn",
         "mocha/no-sibling-hooks": "error",
-        "mocha/no-skipped-tests": "error",
+        "mocha/no-skipped-tests": process.env.CI ? "error" : "warn",
         "mocha/no-top-level-hooks": "error",
         "mocha/no-async-describe": "error",
     }
