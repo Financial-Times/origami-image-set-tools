404e059ad39d0c2bdd2b8f7ea02acba3e63e309d
Jake Champion <jakechampion.jake2@googlemail.com>
2019-03-28 13:32:16 +0000

    update assertion to check that the message includes the expected message. We need to change the assertion type because it also outputs the help message now.

--- test/integration/cli-help.test.js
+++ test/integration/cli-help.test.js
@@ -45,7 +45,7 @@ describe('oist not-a-command', () => {
 	});
 
 	it('outputs an error', () => {
-		assert.strictEqual(global.cliCall.lastResult.output, 'Command "not-a-command" not found');
+		assert.include(global.cliCall.lastResult.output, 'Command "not-a-command" not found');
 	});
 
 	it('exits with a code of 1', () => {
