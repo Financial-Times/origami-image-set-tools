efa10f2a46bf6fd3e32b53762c53fa382969ebc2
Jake Champion <me@jakechampion.name>
2019-04-11 09:49:07 +0100

    Do not upload if file already exists in s3 (#29)
    * Check if file exists on s3 and if it does, do not re-upload it because we should never be overwriting files as files are stored under the hash of their contents and should never collide
    
    * update the mock to include headObject method now that we are using it
    
    * make the unit tests pass

--- test/unit/mock/aws-sdk.mock.js
+++ test/unit/mock/aws-sdk.mock.js
@@ -6,11 +6,16 @@ const AWS = module.exports = {};
 
 AWS.S3 = sinon.stub();
 AWS.S3.mockInstance = {
-	upload: sinon.stub()
+	upload: sinon.stub(),
+	headObject: sinon.stub()
 };
 AWS.S3.mockUpload = {
+	promise: sinon.stub().resolves()
+};
+AWS.S3.headObject = {
 	promise: sinon.stub().resolves()
 };
 
 AWS.S3.returns(AWS.S3.mockInstance);
 AWS.S3.mockInstance.upload.returns(AWS.S3.mockUpload);
+AWS.S3.mockInstance.headObject.returns(AWS.S3.headObject);
