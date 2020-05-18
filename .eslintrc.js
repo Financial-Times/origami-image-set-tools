ba67c81412652f1148fe872c85aac3038caff56c
notlee <lee.moody@ft.com>
2020-05-18 10:40:52 +0100

    Use single quotes and tabs in eslint conf

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,28 @@
 module.exports = {
-    "parserOptions": {
-        "ecmaVersion": 8,
-    },
-    "env": {
-        "es6": true,
-        "node": true,
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+	'parserOptions': {
+		'ecmaVersion': 8,
+	},
+	'env': {
+		'es6': true,
+		'node': true,
+	},
+	'extends': 'eslint:recommended',
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
 };
